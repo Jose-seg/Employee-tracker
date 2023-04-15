bfc2fc88c96013c67a38a6eabe4df07fff698aa5
Jose Segura <jossegura2023@u.northwestern.edu>
2023-04-15 09:05:18 -0500

    added required packages and some folders that are needed

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+// connection to SQL using MYSQL2 paackage:
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Password1',
+    database: 'employee_db'
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to the employee_db database.');
+});
+
+module
