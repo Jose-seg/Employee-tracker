ddc09ec7f4c6fcf042cddf948bf3fb045b48a3eb
Jose Segura <jossegura2023@u.northwestern.edu>
2023-04-18 20:11:40 -0500

    added content to readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Employee-tracker
+
+## Description 
+This app runs in the backend and demonstrates my capabilities of building an employee tracker. This assignment helped me grasped a better understanding for how routes are handled and how retrieve neccessary data through these routes. I was able to get a better understanding of more complicated ways of building the database and who to store as well as present that data.
+
+## Usage
+The purpose of this app is to provide the user with a series of prompts that allow the user to view all data in certain categories such as departments, employees, and roles. A user can also add a department and add an employee, and if needed to the user can update an employee's information in the instance that employee recieved a promotion. 
+
+## Installations
+ - Inquirer
+ - MySQL2
+ - console.table
+
+ ## Video Walkthrough (Link)
+
+ ## Github Repo
+ 
--- index.js
+++ index.js
@@ -26,17 +26,54 @@ async function main() {
 
         // Here a call to appropriate function according to the choice picked by the user
         if (action === 'View all departments') {
-
+            const departments = await queries.viewAllDepartments();
+            console.table(departments);
         } else if (action === 'View all roles') {
-
+            const roles = await queries.viewAllRoles();
+            console.table(roles);
         } else if (action === 'View all employees') {
-
+            const employees = await queries.viewAllEmployees();
+            console.table(employees);
         } else if (action === 'Add a department') {
-
+            const { departmentName } = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'departmentName',
+                    message: 'Enter the name of the new department:',
+                },
+            ]);
+            await addDepartment(departmentName);
+            console.log('Department added successfully.');
         } else if (action === 'Add a role') {
+            const departments = await viewAllDepartments();
+            const departmentChoices = departments.map(department => ({ name: department.name, value: department.id}));
 
+            const { title, salary, departmentId } = await inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'title',
+                    message: 'Enter the title of the new role:',
+                },
+                {
+                    type: 'input',
+                    name: 'salary',
+                    message: 'Enter the salary for the new role:',
+                },
+                {
+                    type: 'list',
+                    name: 'departmentId',
+                    message: 'Select the department for the new role:',
+                    choices: departmentChoices,
+                },
+            ]);
+            await addRole(title, salary, departmentId);
+            console.log('Role added successfully.');
         } else if (action === 'Add an employee') {
+            const roles = await viewAllRoles();
+            const roleChoices = roles.map(role => ({ name: role.job_title, value: role.role_id}));
 
+            const employees = await viewAllEmployees();
+            const managerChoices = employees.map(employee => ({ }))
         } else if (action === 'Update an employee role') {
 
         } else {
